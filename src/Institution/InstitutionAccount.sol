dc462ae4a359563955047a72db3091443916d1bd
torof <scottpanam@protonmail.com>
2023-09-16 23:02:47 +0900

    factory

--- src/Institution/InstitutionAccount.sol
+++ src/Institution/InstitutionAccount.sol
@@ -2,11 +2,11 @@
 
 pragma solidity 0.8.21;
 
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
-contract InstitutionAccount is ERC1155, Ownable2Step {
+contract InstitutionAccount is ERC1155Supply, Ownable2Step {
     using Strings for uint256;
     constructor(string memory _uri) ERC1155(_uri){}
 
